cd9a74159816af868c8508c3e9f1811b2b007e48
tomergros <77513087+tomergros@users.noreply.github.com>
2023-07-17 17:21:25 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -1,32 +1,28 @@
 import streamlit as st
 import requests
 
-def fetch_kaggle_notebook(notebook_id):
-    url = f"https://www.kaggleusercontent.com/api/notebooks/{notebook_id}/export?format=json"
+def fetch_kaggle_notebook(username, notebook_slug):
+    url = f"https://www.kaggle.com/{username}/{notebook_slug}/download"
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()
+        return response.content.decode("utf-8")
     return None
 
 def main():
-    st.title("XML- Firewall: Cyber Attacks Classification")
+    st.title("Kaggle Notebook Viewer")
 
-    # Hardcoded Kaggle notebook ID
-    notebook_id = '136941707'
+    # Hardcoded Kaggle username and notebook slug
+    username = 'tomergrossy'
+    notebook_slug = 'xml-firewall-cyber-attacks-classification'
 
     # Fetch the Kaggle notebook content
-    notebook_data = fetch_kaggle_notebook(notebook_id)
+    notebook_content = fetch_kaggle_notebook(username, notebook_slug)
 
-    if notebook_data:
-        # Display the notebook code
-        code_blocks = notebook_data["cells"]
-        for code_block in code_blocks:
-            if code_block["cell_type"] == "code":
-                code_lines = code_block["source"]
-                code = "\n".join(code_lines)
-                st.code(code, language="python")
+    if notebook_content:
+        # Display the notebook content
+        st.code(notebook_content, language="python")
     else:
-        st.error("Notebook not found. Please check the notebook ID.")
+        st.error("Notebook not found. Please check the Kaggle username and notebook slug.")
 
 if __name__ == '__main__':
     main()
