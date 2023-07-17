d507d8669c0c92c0e74b7862afd7318845c6e0f1
tomergros <77513087+tomergros@users.noreply.github.com>
2023-07-17 17:44:04 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -1,37 +1,18 @@
 import streamlit as st
-import kaggle
-
-def download_kaggle_notebook(username, notebook_slug):
-    kaggle.api.dataset_download_files(f'{username}/{notebook_slug}', path='.', unzip=True)
-    files = os.listdir('.')
-    for file in files:
-        if file.endswith('.ipynb'):
-            return file
-    return None
+from IPython.display import IFrame
 
 def main():
     st.title("Kaggle Notebook Viewer")
 
-    # Hardcoded Kaggle username and notebook slug
-    username = 'tomergrossy'
-    notebook_slug = 'xml-firewall-cyber-attacks-classification'
+    # Hardcoded Kaggle notebook URL
+    notebook_url = 'https://www.kaggle.com/code/tomergrossy/xml-firewall-cyber-attacks-classification'
 
-    # Download the Kaggle notebook
-    notebook_file = download_kaggle_notebook(username, notebook_slug)
-
-    if notebook_file:
-        # Read the notebook file
-        with open(notebook_file, 'r', encoding='utf-8') as file:
-            notebook_content = file.read()
-
-        # Display the notebook content
-        st.code(notebook_content, language="python")
-    else:
-        st.error("Notebook not found. Please check the Kaggle username and notebook slug.")
-
-if __name__ == '__main__':
-    main()
-
+    # Display the Kaggle notebook
+    st.markdown(f"XML- Firewall: Cyber Attacks Classification")
+    st.markdown(
+        f'<iframe src="{notebook_url}" width="800" height="600" frameborder="0"></iframe>',
+        unsafe_allow_html=True
+    )
 
 if __name__ == '__main__':
     main()
