f33ab854830791475f4d1d82ff8146babc0bcf24
tomergros <77513087+tomergros@users.noreply.github.com>
2023-07-17 18:28:45 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -4,7 +4,7 @@ def main():
     st.title('xml-firewall-cyber-attacks-classification')
     
     # Add an iframe to display the Kaggle notebook
-    st.components.v1.iframe("https://github.com/tomergros/xml-firewall-streamlit/blob/main/xml-firewall-cyber-attacks-classification.html", height=1000)
+    st.components.v1.iframe("https://raw.githubusercontent.com/tomergros/xml-firewall-streamlit/main/xml-firewall-cyber-attacks-classification.html", height=1000)
     
 if __name__ == '__main__':
     main()
