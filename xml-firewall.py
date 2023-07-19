d4fc70356a0d858c890f8c80734574deb764cdc2
tomergros <77513087+tomergros@users.noreply.github.com>
2023-07-19 16:10:32 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -21,24 +21,34 @@ def main():
         layout="wide",
     )
 
-    # Centering the titles
+    # Uncomment the CSS style to center the titles and button
     st.markdown(
         """
         <style>
         .title {
             text-align: center;
         }
+        .upload-title {
+            font-size: 20px;
+        }
+        .center {
+            display: flex;
+            justify-content: center;
+        }
+        .validate-button {
+            font-size: 24px;
+            padding: 12px 30px;
+        }
         </style>
         """,
         unsafe_allow_html=True,
     )
 
-    # st.title("Kaggle Notebook Viewer and XML Firewall")
-    st.markdown("<h1 style='color: #ffffff; text-align: center;'>Kaggle Notebook Viewer and XML Firewall</h1>", unsafe_allow_html=True)
+    st.title("Kaggle Notebook Viewer and XML Firewall")
 
     # Kaggle Notebook Viewer
     st.markdown("<h1 style='color: #ff6347; text-align: center;'>Kaggle Notebook Viewer</h1>", unsafe_allow_html=True)
-    html_file_path = r"xml-firewall-cyber-attacks-classification.html"
+    html_file_path = "xml-firewall-streamlit-main\xml-firewall-cyber-attacks-classification.html"
     try:
         with open(html_file_path, "r", encoding="utf-8") as file:
             html_content = file.read()
@@ -49,14 +59,15 @@ def main():
         st.error("An error occurred while loading the Kaggle notebook HTML content.")
 
     # XML Firewall
-    # st.title("XML Firewall")
-    st.markdown("<h1 style='color: #ff6347; text-align: center;'>XML Firewall</h1>", unsafe_allow_html=True)
+    st.title("XML Firewall")
 
-    st.markdown("<p class='upload-title' style='text-align: center; font-size: 40px;'>Upload XSD File</p>", unsafe_allow_html=True)
+    st.markdown("<p class='upload-title' style='text-align: center; font-size: 30px;'>Upload XSD File</p>", unsafe_allow_html=True)
     xsd_file = st.file_uploader("", type=["xsd"])
 
-    st.markdown("<p class='upload-title' style='text-align: center; font-size: 40px;'>Upload XML File</p>", unsafe_allow_html=True)
+    st.markdown("<p class='upload-title' style='text-align: center; font-size: 30px;'>Upload XML File</p>", unsafe_allow_html=True)
     xml_file = st.file_uploader("", type=["xml"])
+
+    st.markdown("<div class='center'><button class='validate-button'>Validate</button></div>", unsafe_allow_html=True)
 
     if st.button("Validate"):
         if not xsd_file or not xml_file:
