be654f198ef16ca6089a3965975a4f5587aaec03
tomergros <77513087+tomergros@users.noreply.github.com>
2023-07-19 17:24:51 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -71,6 +71,10 @@ def main():
     # st.title("Kaggle Notebook Viewer and XML Firewall")
     st.markdown("<h1 style='color: #ffffff; text-align: center;'>Kaggle Notebook Viewer</h1>", unsafe_allow_html=True)
 
+    # Insert the image below the title
+    image_path = "xml-firewall-logo.jpg"
+    st.image(image_path, use_column_width=True)
+    
     # Kaggle Notebook Viewer
     st.markdown("<h1 style='color: #ff6347; text-align: center;'>Kaggle Notebook Viewer</h1>", unsafe_allow_html=True)
     html_file_path = r"xml-firewall-cyber-attacks-classification.html"
