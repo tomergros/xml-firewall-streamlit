ab6833a76ff63b5f6135feb1d815ff0960c5f666
tomergros <77513087+tomergros@users.noreply.github.com>
2023-07-29 21:58:49 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -80,7 +80,7 @@ def main():
     iframe_code = f'<iframe src="{iframe_url}" height="2000" style="margin: 0 auto; width: 100%; max-width: 2000px;" frameborder="0" scrolling="auto" title="XML- Firewall: Cyber Attacks Classification"></iframe>'
 
     # Kaggle Notebook Viewer
-    st.markdown("<h1 style='color: #ff6347; text-align: center;'>Kaggle Notebook Viewer</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color: #ff6347; text-align: center;'>Kaggle Notebook Viewer</h1>")
     st.write(iframe_code, unsafe_allow_html=True)    
 
     # html_file_path = r"xml-firewall-cyber-attacks-classification.html"
