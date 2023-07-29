45059d5de03b9aabad8e77beb19b435ebc826d9f
tomergros <77513087+tomergros@users.noreply.github.com>
2023-07-29 16:44:33 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -94,6 +94,13 @@ def main():
     #     st.error("An error occurred while loading the Kaggle notebook HTML content.")
 
     # XML Firewall
+    st.header("Basic Instructions:")
+    st.markdown("1. Click on the arrow to start the app.")
+    st.markdown("2. Browse an XSD file.")
+    st.markdown("3. Browse an XML file.")
+    st.markdown("4. Click on 'Validate'.")
+    st.markdown("That's it! You are ready to go!")
+    
     # st.title("XML Firewall")
     st.markdown("<h1 style='color: #ffffff; text-align: center;'>XML Firewall</h1>", unsafe_allow_html=True)
 
