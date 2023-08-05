08c89bebbddfbf24593630ad769c009671d17e67
tomergros <77513087+tomergros@users.noreply.github.com>
2023-08-05 15:46:47 +0300

    Update xml-firewall.py

--- xml-firewall.py
+++ xml-firewall.py
@@ -127,15 +127,15 @@ def main():
     st.video(video_url)
 
     image_path = "pics/basic_terms3png.png"
+    st.markdown("<h5 style='color: #ffffff; text-align: center;'>SQL Injection Example</h5>", unsafe_allow_html=True)
     st.image(image_path, use_column_width=True)
-    st.markdown("<h6 style='color: #ffffff; text-align: center;'>SQL Injection Example</h6>", unsafe_allow_html=True)
 
     # Video URL or local file path
     video_url = "pics/DDOS attack on the VideoLAN downloads infrastructure.mp4"  # Replace this with your video URL or local file path
     
     # Display the video
+    st.markdown("<h5 style='color: #ffffff; text-align: center;'>DOS Example</h5>", unsafe_allow_html=True)
     st.video(video_url)
-    st.markdown("<h6 style='color: #ffffff; text-align: center;'>DOS Example</h6>", unsafe_allow_html=True)
 
     image_path = "pics/basic_terms4.png"
     st.image(image_path, use_column_width=True)
